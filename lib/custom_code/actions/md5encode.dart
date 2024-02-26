6bb561a3c58518606447a1220dc17ae46840f939
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-02-26 05:02:13 +0000

    Updating to latest FlutterFlow output.

--- lib/custom_code/actions/md5encode.dart
+++ lib/custom_code/actions/md5encode.dart
@@ -7,11 +7,12 @@ import 'package:flutter/material.dart';
 // Begin custom action code
 // DO NOT REMOVE OR MODIFY THE CODE ABOVE!
 
+import 'dart:convert';
+
 import 'package:crypto/crypto.dart';
 
 Future<String> md5encode(String? senha) async {
   // create a md5 md5encode that returns the String i give, but in md5 encoded
-  if (senha == null) return null;
 
   final bytes = utf8.encode(senha);
   final digest = md5.convert(bytes);
