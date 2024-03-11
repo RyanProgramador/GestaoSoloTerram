b249d580914c642bbab2d17ef9a738b619850144
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-03-11 12:05:52 +0000

    Updating to latest FlutterFlow output.

--- lib/components/troca_url_widget.dart
+++ lib/components/troca_url_widget.dart
@@ -127,113 +127,183 @@ class _TrocaUrlWidgetState extends State<TrocaUrlWidget> {
                       _model.textControllerValidator.asValidator(context),
                 ),
               ),
-              Align(
-                alignment: const AlignmentDirectional(1.0, 1.0),
-                child: FFButtonWidget(
-                  onPressed: () async {
-                    var shouldSetState = false;
-                    var confirmDialogResponse = await showDialog<bool>(
-                          context: context,
-                          builder: (alertDialogContext) {
-                            return AlertDialog(
-                              title: const Text('Atenção'),
-                              content: const Text(
-                                  'Deseja realmente alterar a API do aplicativo?'),
-                              actions: [
-                                TextButton(
-                                  onPressed: () =>
-                                      Navigator.pop(alertDialogContext, false),
-                                  child: const Text('Não'),
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Align(
+                    alignment: const AlignmentDirectional(1.0, 1.0),
+                    child: FFButtonWidget(
+                      onPressed: () async {
+                        var confirmDialogResponse = await showDialog<bool>(
+                              context: context,
+                              builder: (alertDialogContext) {
+                                return AlertDialog(
+                                  title: const Text('Atenção'),
+                                  content:
+                                      const Text('Deseja realmente limpar a URL?'),
+                                  actions: [
+                                    TextButton(
+                                      onPressed: () => Navigator.pop(
+                                          alertDialogContext, false),
+                                      child: const Text('Não'),
+                                    ),
+                                    TextButton(
+                                      onPressed: () => Navigator.pop(
+                                          alertDialogContext, true),
+                                      child: const Text('Sim'),
+                                    ),
+                                  ],
+                                );
+                              },
+                            ) ??
+                            false;
+                        if (confirmDialogResponse) {
+                          FFAppState().update(() {
+                            FFAppState().UrlApi =
+                                '://170.238.54.36:8090/terram/api/index.php';
+                          });
+                        } else {
+                          return;
+                        }
+                      },
+                      text: 'Limpar',
+                      options: FFButtonOptions(
+                        width: 100.0,
+                        height: 40.0,
+                        padding:
+                            const EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 0.0, 0.0),
+                        iconPadding:
+                            const EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 0.0, 0.0),
+                        color: const Color(0xFF9D291C),
+                        textStyle:
+                            FlutterFlowTheme.of(context).bodyLarge.override(
+                                  fontFamily: 'Readex Pro',
+                                  color: FlutterFlowTheme.of(context)
+                                      .primaryBackground,
                                 ),
-                                TextButton(
-                                  onPressed: () =>
-                                      Navigator.pop(alertDialogContext, true),
-                                  child: const Text('Sim'),
-                                ),
-                              ],
-                            );
-                          },
-                        ) ??
-                        false;
-                    if (confirmDialogResponse) {
-                      _model.validaApi =
-                          await TrOsServicosGroup.ffValidaApiCall.call(
-                        urlApi: _model.textController.text,
-                        senha: 'asdasd',
-                        usuario: 'asdasd',
-                      );
-                      shouldSetState = true;
-                      if (!getJsonField(
-                        (_model.validaApi?.jsonBody ?? ''),
-                        r'''$.status''',
-                      )) {
-                        await showDialog(
-                          context: context,
-                          builder: (alertDialogContext) {
-                            return AlertDialog(
-                              title: const Text('Ops!'),
-                              content: const Text('URL invalida.'),
-                              actions: [
-                                TextButton(
-                                  onPressed: () =>
-                                      Navigator.pop(alertDialogContext),
-                                  child: const Text('Entendi'),
-                                ),
-                              ],
-                            );
-                          },
-                        );
-                        if (shouldSetState) setState(() {});
-                        return;
-                      }
-                      setState(() {
-                        FFAppState().UrlApi = functions
-                            .protocoloComSeguranca(_model.textController.text)!;
-                      });
-                      await showDialog(
-                        context: context,
-                        builder: (alertDialogContext) {
-                          return AlertDialog(
-                            title: const Text('Sucesso!'),
-                            content: const Text('URL alterada com sucesso!'),
-                            actions: [
-                              TextButton(
-                                onPressed: () =>
-                                    Navigator.pop(alertDialogContext),
-                                child: const Text('Ok'),
-                              ),
-                            ],
-                          );
-                        },
-                      );
-                      Navigator.pop(context);
-                    } else {
-                      if (shouldSetState) setState(() {});
-                      return;
-                    }
-
-                    if (shouldSetState) setState(() {});
-                  },
-                  text: 'Salvar',
-                  options: FFButtonOptions(
-                    width: 100.0,
-                    height: 40.0,
-                    padding: const EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 0.0, 0.0),
-                    iconPadding:
-                        const EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 0.0, 0.0),
-                    color: FlutterFlowTheme.of(context).primary,
-                    textStyle: FlutterFlowTheme.of(context).bodyLarge.override(
-                          fontFamily: 'Readex Pro',
-                          color: FlutterFlowTheme.of(context).primaryBackground,
+                        elevation: 2.0,
+                        borderSide: const BorderSide(
+                          color: Colors.transparent,
+                          width: 1.0,
                         ),
-                    elevation: 2.0,
-                    borderSide: const BorderSide(
-                      color: Colors.transparent,
-                      width: 1.0,
+                        borderRadius: BorderRadius.circular(24.0),
+                      ),
                     ),
-                    borderRadius: BorderRadius.circular(24.0),
                   ),
-                ),
+                  Align(
+                    alignment: const AlignmentDirectional(1.0, 1.0),
+                    child: FFButtonWidget(
+                      onPressed: () async {
+                        var shouldSetState = false;
+                        var confirmDialogResponse = await showDialog<bool>(
+                              context: context,
+                              builder: (alertDialogContext) {
+                                return AlertDialog(
+                                  title: const Text('Atenção'),
+                                  content: const Text(
+                                      'Deseja realmente alterar a API do aplicativo?'),
+                                  actions: [
+                                    TextButton(
+                                      onPressed: () => Navigator.pop(
+                                          alertDialogContext, false),
+                                      child: const Text('Não'),
+                                    ),
+                                    TextButton(
+                                      onPressed: () => Navigator.pop(
+                                          alertDialogContext, true),
+                                      child: const Text('Sim'),
+                                    ),
+                                  ],
+                                );
+                              },
+                            ) ??
+                            false;
+                        if (confirmDialogResponse) {
+                          _model.validaApi =
+                              await TrOsServicosGroup.ffValidaApiCall.call(
+                            urlApi: _model.textController.text,
+                            senha: 'asdasd',
+                            usuario: 'asdasd',
+                          );
+                          shouldSetState = true;
+                          if (!getJsonField(
+                            (_model.validaApi?.jsonBody ?? ''),
+                            r'''$.status''',
+                          )) {
+                            await showDialog(
+                              context: context,
+                              builder: (alertDialogContext) {
+                                return AlertDialog(
+                                  title: const Text('Ops!'),
+                                  content: const Text('URL invalida.'),
+                                  actions: [
+                                    TextButton(
+                                      onPressed: () =>
+                                          Navigator.pop(alertDialogContext),
+                                      child: const Text('Entendi'),
+                                    ),
+                                  ],
+                                );
+                              },
+                            );
+                            if (shouldSetState) setState(() {});
+                            return;
+                          }
+                          setState(() {
+                            FFAppState().UrlApi =
+                                functions.protocoloComSeguranca(
+                                    _model.textController.text)!;
+                          });
+                          await showDialog(
+                            context: context,
+                            builder: (alertDialogContext) {
+                              return AlertDialog(
+                                title: const Text('Sucesso!'),
+                                content: const Text('URL alterada com sucesso!'),
+                                actions: [
+                                  TextButton(
+                                    onPressed: () =>
+                                        Navigator.pop(alertDialogContext),
+                                    child: const Text('Ok'),
+                                  ),
+                                ],
+                              );
+                            },
+                          );
+                          Navigator.pop(context);
+                        } else {
+                          if (shouldSetState) setState(() {});
+                          return;
+                        }
+
+                        if (shouldSetState) setState(() {});
+                      },
+                      text: 'Salvar',
+                      options: FFButtonOptions(
+                        width: 100.0,
+                        height: 40.0,
+                        padding:
+                            const EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 0.0, 0.0),
+                        iconPadding:
+                            const EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 0.0, 0.0),
+                        color: FlutterFlowTheme.of(context).primary,
+                        textStyle:
+                            FlutterFlowTheme.of(context).bodyLarge.override(
+                                  fontFamily: 'Readex Pro',
+                                  color: FlutterFlowTheme.of(context)
+                                      .primaryBackground,
+                                ),
+                        elevation: 2.0,
+                        borderSide: const BorderSide(
+                          color: Colors.transparent,
+                          width: 1.0,
+                        ),
+                        borderRadius: BorderRadius.circular(24.0),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
