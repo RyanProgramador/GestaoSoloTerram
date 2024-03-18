da9302b06fc3fe52b720e37a31e00bd2715b2367
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-03-18 19:59:48 +0000

    Updating to latest FlutterFlow output.

--- lib/custom_code/widgets/qrcode_scanner.dart
+++ lib/custom_code/widgets/qrcode_scanner.dart
@@ -9,6 +9,8 @@ import 'package:flutter/material.dart';
 // Begin custom widget code
 // DO NOT REMOVE OR MODIFY THE CODE ABOVE!
 
+import '/flutter_flow/flutter_flow_icon_button.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
 
 import '../../criacao_volume/criacao_volume_model.dart';
@@ -34,10 +36,12 @@ class _QrcodeScannerState extends State<QrcodeScanner> {
 
   QRViewController? controller;
   String qrText = '';
+  final TextEditingController textController = TextEditingController();
 
   @override
   void dispose() {
     controller?.dispose();
+    textController.dispose();
     super.dispose();
   }
 
@@ -45,33 +49,83 @@ class _QrcodeScannerState extends State<QrcodeScanner> {
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        Center(
-          child: SizedBox(
-            width: double.infinity,
-            height: double.infinity,
-            child: QRView(
-              key: qrKey,
-              onQRViewCreated: _onQRViewCreated,
-              overlay: QrScannerOverlayShape(
-                borderColor: Color(0xFF00736D),
-                borderRadius: 10,
-                borderLength: 130,
-                borderWidth: 5,
-                overlayColor: Colors.white,
+        QRView(
+          key: qrKey,
+          onQRViewCreated: _onQRViewCreated,
+          overlay: QrScannerOverlayShape(
+            borderColor: Color(0xFF00736D),
+            borderRadius: 10,
+            borderLength: 130,
+            borderWidth: 5,
+            overlayColor: Colors.white,
+          ),
+        ),
+        Positioned(
+          bottom: -30,
+          left: 0,
+          right: 0,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Text(
+                    'Código',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ],
               ),
-            ),
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Expanded(
+                    child: Padding(
+                      padding: EdgeInsets.only(right: 8),
+                      child: TextFormField(
+                        controller: textController,
+                        obscureText: false,
+                        decoration: InputDecoration(
+                          labelText: 'Código',
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Color(0xFF00736d),
+                              width: 2,
+                            ),
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Color(0xFF00736d),
+                              width: 2,
+                            ),
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  FloatingActionButton(
+                    mini: true,
+                    onPressed: () {
+                      // Your button press logic here
+                    },
+                    child: Icon(Icons.arrow_forward, color: Colors.white),
+                    backgroundColor: Color(0xFF00736d),
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
         Positioned(
           left: 10,
           bottom: 10,
           child: FloatingActionButton(
-            onPressed: () {
-              controller
-                  ?.resumeCamera(); // Call resumeCamera on the QRView controller
-            },
+            onPressed: () => controller?.resumeCamera(),
             child: Icon(Icons.delete, color: Colors.white),
-            backgroundColor: Colors.red,
+            backgroundColor: Color(0xFF982c26),
           ),
         ),
       ],
@@ -83,9 +137,11 @@ class _QrcodeScannerState extends State<QrcodeScanner> {
     controller.scannedDataStream.listen((scanData) {
       setState(() {
         qrText = scanData.code!;
-        FFAppState().teste.add(qrText);
+        // FFAppState().teste.add(qrText);
         controller?.pauseCamera();
-        FFAppState().numeroVolumeQrCode = qrText;
+        // FFAppState().numeroVolumeQrCode = qrText;
+        // context.goNamed('criacaoVolume',);
+        textController.text = scanData.code!;
         // Potentially perform an action based on the scanned QR code
         print('Scanned QR code: $qrText'); // Example action: log to console
       });
