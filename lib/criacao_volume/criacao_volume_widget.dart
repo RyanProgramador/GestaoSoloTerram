c5f5f23b88d1fb5dee05e3538c3f46de9594aa4a
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-03-25 13:57:45 +0000

    Updating to latest FlutterFlow output.

--- lib/criacao_volume/criacao_volume_widget.dart
+++ lib/criacao_volume/criacao_volume_widget.dart
@@ -204,7 +204,7 @@ class _CriacaoVolumeWidgetState extends State<CriacaoVolumeWidget> {
                               hoverColor: Colors.transparent,
                               highlightColor: Colors.transparent,
                               onTap: () async {
-                                Navigator.pop(context);
+                                context.safePop();
                               },
                               child: Icon(
                                 Icons.arrow_back,
